ef6396cd2f6785d9b3cbd5108a459d96c520c95e
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-07-13 16:39:42 +0200

    More refactoring

--- src/datetimeus.jl
+++ src/datetimeus.jl
@@ -1,9 +1,7 @@
-
 import Base.+, Base.show, Base.isless
 
 
 +(x::Dates.UTInstant{Microsecond}, d::Microsecond) = Dates.UTInstant(x.periods + d)
-
 
 function show(io::IO, x::Dates.UTInstant{Microsecond})
     fpart, ipart = modf(1e-3 * x.periods.value)
@@ -13,9 +11,7 @@ function show(io::IO, x::Dates.UTInstant{Microsecond})
     print(io, "$date$usec")
 end
 
-
 show(io::IO, ::MIME"text/plain", x::Dates.UTInstant{Microsecond}) = show(io, x)
-
 
 function DateTimeMicrosecond(y, m, d, h, mi, s, us)
     dt = DateTime(y, m, d, h, mi, s)
@@ -23,8 +19,6 @@ function DateTimeMicrosecond(y, m, d, h, mi, s, us)
     Dates.UTInstant(rata)
 end
 
-
 DateTimeMicrosecond(dt::DateTime) = Dates.UTInstant(convert(Microsecond, dt.instant.periods))
-
 
 isless(t1::Dates.UTInstant{Microsecond}, t2::Dates.UTInstant{Microsecond}) = isless(t1.periods.value, t2.periods.value)
